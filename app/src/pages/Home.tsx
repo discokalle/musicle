b064e2576f66021aa9ae148960f1d55462394fa4
kardu253 <kardu253@student.liu.se>
2025-04-14 15:01:14 +0200

    Created song carousel and song card components.

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,13 +1,29 @@
+import SongCard from "../components/SongCard";
+import SongCarousel from "../components/SongCarousel";
+import markoolioCover from "../assets/markoolio.jpg";
+import tealCover from "../assets/teal.jpg";
+import gxtorCover from "../assets/gxtor.jpg";
+
 function Home() {
   const centerContainerCSS =
-    "absolute flex flex-col gap-5 items-center left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2";
+    "absolute flex flex-col items-center left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2";
 
   const titleCSS =
-    "text-3xl text-neutral text-center transition-transform duration-200 ease-in-out hover:scale-110";
+    "text-3xl text-white text-center transition-transform duration-200 ease-in-out hover:scale-110";
 
   return (
     <div className={centerContainerCSS}>
-      <h1 className={titleCSS}>THIS IS THE HOME PAGE</h1>
+      <h1 className={titleCSS}>Kalles Senaste</h1>
+      <SongCarousel>
+        <SongCard title="Bara Femmor" artist="Gxtsh" cover={gxtorCover} />
+        <SongCard
+          title="Vi drar till fjällen"
+          artist="Markoolio"
+          cover={markoolioCover}
+        />
+        <SongCard title="Africa" artist="Weezer" cover={tealCover} />
+        <SongCard title="Bara Femmor" artist="Gxtsh" cover={gxtorCover} />
+      </SongCarousel>
     </div>
   );
 }
